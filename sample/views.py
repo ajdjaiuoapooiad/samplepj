121530ddfa76a40c4846f1ed4b5f8ef5be490c1d
user <user@usernoMacBook-Air.local>
2024-08-01 16:26:30 +0900

    listview

--- sample/views.py
+++ sample/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def listfunc(request):
+    return render(request,'sample/post_list.html')
+    
--- samplepj/settings.py
+++ samplepj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sample.apps.SampleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
